2314bdbcb8d778702806311b91bc5d4e411988ce
Dmitry Panin <pank.dm@gmail.com>
2019-07-21 10:44:34 -0700

    rust refactoring

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,13 +4,13 @@ use std::str;
 use std::collections::VecDeque;
 use std::time::{Duration, Instant};
 
-type Graph = Vec<Vec<i32>>;
+type Graph = Vec<Vec<usize>>;
 
 #[derive(Debug)]
 struct Input {
     graph: Graph,
     mines: Vec<String>,
-    num_cities: i32,
+    num_cities: usize,
 }
 
 impl Input {
@@ -34,7 +34,7 @@ fn read_from_file(filename: &String) -> Input {
     let mut lines = text.lines();
     let num_cities: i32 = read_i32(&mut lines);
 
-    input.num_cities = num_cities;
+    input.num_cities = num_cities as usize;
     input.graph.resize(num_cities as usize, vec![]);
 
     let num_mines: i32 = read_i32(&mut lines);
@@ -49,10 +49,10 @@ fn read_from_file(filename: &String) -> Input {
     for _ in 0..num_edges {
         let edge: Vec<&str> = lines.next().unwrap().split(" ").collect();
         // println!("{:?}", edge);
-        let s: i32 = edge[0].parse().unwrap();
-        let t: i32 = edge[1].parse().unwrap();
-        input.graph[s as usize].push(t);
-        input.graph[t as usize].push(s);
+        let s: usize = edge[0].parse().unwrap();
+        let t: usize = edge[1].parse().unwrap();
+        input.graph[s].push(t);
+        input.graph[t].push(s);
     }
 
     input
@@ -60,20 +60,20 @@ fn read_from_file(filename: &String) -> Input {
 
 const NOT_PROCESSED: i32 = -1;
 
-fn run_bfs(start: i32, input: &Input, scores: &mut Vec<i32>) {
-    scores.resize(input.num_cities as usize, NOT_PROCESSED);
-    scores[start as usize] = 0;
+fn run_bfs(start: usize, input: &Input, scores: &mut Vec<i32>) {
+    scores.resize(input.num_cities, NOT_PROCESSED);
+    scores[start] = 0;
 
-    let mut queue: VecDeque<i32> = VecDeque::new();
+    let mut queue: VecDeque<usize> = VecDeque::new();
     queue.push_back(start);
 
     while !queue.is_empty() {
         let now = queue.pop_front().unwrap();
-        let value = scores[now as usize];
+        let value = scores[now];
 
-        for next in input.graph[now as usize].iter() {
-            if scores[*next as usize] == NOT_PROCESSED {
-                scores[*next as usize] = value + 1;
+        for next in &input.graph[now] {
+            if scores[*next] == NOT_PROCESSED {
+                scores[*next] = value + 1;
                 queue.push_back(*next);
             }
         }
@@ -85,16 +85,18 @@ fn compute_hash(input: &Input) {
    let now = Instant::now();
 
     let mut distances: Vec<Vec<i32>> = vec![];
-    distances.resize(input.num_cities as usize, vec![]);
+    distances.resize(input.num_cities, vec![]);
 
     for i in 0..input.num_cities {
-        run_bfs(i, &input, &mut distances[i as usize]);
+        let node = i as usize;
+        run_bfs(node, &input, &mut distances[node]);
     }
 
     let mut total_score: i32 = 0;
     for i in 0..input.num_cities {
         let mut current_score: i32 = 0;
-        for d in distances[i as usize].iter() {
+        let node = i as usize;
+        for d in &distances[node] {
             if *d == NOT_PROCESSED {
                 continue;
             }
